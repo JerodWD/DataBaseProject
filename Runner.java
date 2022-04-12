94728df39fcb1a5aaf25baa6fccbcc4e69290b21
Androidrox <tylerfreeman357@gmail.com>
2022-04-12 11:23:12 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -19,6 +19,8 @@ import java.sql.*;
 
 public class Runner {
 	public static String currentTable;
+	public static JFrame secondWindow;
+	public static JPanel textPanel;
 
 	public static void main(String[] args) {
 
@@ -69,19 +71,19 @@ public class Runner {
 		//adding components to mainWindow
 		mainWindow.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 		mainWindow.getContentPane().add(test, BorderLayout.SOUTH);
-		
+
 		//window size and design
 		mainWindow.setVisible(true);
 
 
 		//creates secondary window for specific table viewing
-		JFrame secondWindow = new JFrame("TableViewer");
+		secondWindow = new JFrame("TableViewer");
 		secondWindow.setPreferredSize(new Dimension(1200, 600));
 		secondWindow.pack();
 
 		//attempt to add scroll panel to text box for table output
 
-		JPanel textPanel = new JPanel();
+		textPanel = new JPanel();
 
 		//creates buttons for secondary window
 		JButton back = new JButton("Back");
@@ -118,127 +120,96 @@ public class Runner {
 
 				//a public value that is set depending on what table is being accesses
 				currentTable = "CUSTOMER";
-
-				//calls the getColumns method which uses JDBC to return a tables number of columns
-				int columns = getColumns();
-
-				//calls the getRows method which uses JDBC to return a tables number of rows
-				int rows = getRows();
-
-				//hard coded array for creation of the JTable.  Eventually make into a method that pulls out the name of every column name so I can put data grabbing from database
-				//into a method of its own
-				String columnNames[] = {"CUST_ID", "CUST_FNAME", "CUST_LNAME", "CUST_ADDRESS", "CUST_CITY", "CUST_STATE", "CUST_ZIP", "CUST_PHONENUM", "CUST_SEATNUM", "CUST_AGE", "FLIGHT_NUM"};
-
-				//creation of 2d array that takes in the values from the Customer Table from the database
-				Object data[][] = new String[rows][columns];
-
-				//try catch that uses the JDBC
-				try (
-						Connection conn = DriverManager.getConnection(
-								"jdbc:mysql://localhost:3306/flight manager?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
-								"root", "root");   // For MySQL only
-						Statement stmt = conn.createStatement();
-						) {
-					//creates a query in the database to grab all values from the Customer Table
-					String strSelect = "select * from " + currentTable;
-					ResultSet rset = stmt.executeQuery(strSelect);
-					
-					//variables used to add data to the 2D array
-					int column = 0;
-					int row = 0;
-
-					//data is printed out row by row, loops through the data grabbed from database and adds it to the 2D array
-					//this loop skips to the next row each time it iterates
-					while(rset.next()) { 
-						//loop for adding the data to each column, loops 11 times for each row
-						while(column != columns) {
-							data[row][column] = rset.getString(column+1);
-							column++;
-						}
-						//resets column data for the next row, increase row with the while loop
-						column = 0;
-						row++;
-					} 
-
-				} catch(SQLException ex) {
-					ex.printStackTrace();
-				}
-
-				//creates the JTable the data in inputed to
-				JTable dataTable = new JTable(data, columnNames);
 				
-				//adds a header to remain visible wherever the user is scrolled to in the table
-				JTableHeader header = dataTable.getTableHeader();
+				//a method that prints out the current table data based on the value 'currentTable'
+				printData();
 				
-				//sets the header background to green
-				header.setBackground(Color.green);
-				
-				//creates a scroll bar to be added to the table
-				JScrollPane scrollPane = new JScrollPane(dataTable);
-				
-				//sets the table dimensions in the window, resizing is off so buttons aren't covered
-				dataTable.setPreferredScrollableViewportSize(new Dimension(825,520));
-				dataTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-				
-				//adds scroll bar to table so user can see all data
-				textPanel.add(scrollPane);
-				
-				//sets how many rows and columns are visible, along with coordinates of the table (top left for our case)
-				dataTable.setBounds(0,0,10,20);
-				scrollPane.setViewportView(dataTable);
-				
-				//adds the table to the window
-				secondWindow.add(textPanel, BorderLayout.WEST);
 				//opens the new window with the table
 				secondWindow.setVisible(true);
-				
+
 			}
 		});
 
 		plane.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				//remove the main window from screen
 				mainWindow.setVisible(false);
+				
+				//names the new window Plane Table
+				secondWindow.setTitle("Plane Table");
+				
+				//sets the public table value to plane
 				currentTable = "Plane";
+				
+				//a method that prints out the current table data based on the value 'currentTable'
+				printData();
+				
+				//sets the second window to true
 				secondWindow.setVisible(true);
 			}
 		});
 
 		flight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 				mainWindow.setVisible(false);
+				
 				currentTable = "Flight";
+				
+				printData();
+				
 				secondWindow.setVisible(true);
 			}
 		});
 
 		route.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 				mainWindow.setVisible(false);
+				
 				currentTable = "Route";
+				
+				printData();
+				
 				secondWindow.setVisible(true);
 			}
 		});
 
 		airport.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 				mainWindow.setVisible(false);
+				
 				currentTable = "Airport";
+				
+				printData();
+				
 				secondWindow.setVisible(true);
 			}
 		});
 
 		flightAtt.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 				mainWindow.setVisible(false);
-				currentTable = "FlightAtt";
+				
+				currentTable = "Flight_attendant";
+				
+				printData();
+				
 				secondWindow.setVisible(true);
 			}
 		});
 
 		captain.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 				mainWindow.setVisible(false);
+				
 				currentTable = "Captain";
+				
+				printData();
+				
 				secondWindow.setVisible(true);
 			}
 		});
@@ -247,24 +218,25 @@ public class Runner {
 			public void actionPerformed(ActionEvent arg0) {
 				//closes table window
 				secondWindow.setVisible(false);
-				
+
 				//removes table from textPanel
 				textPanel.removeAll();
-				
+
 				//updates the window
 				secondWindow.revalidate();
 				secondWindow.repaint();
-				
+
 				//sets current table to nothing
 				currentTable = "";
-				
-				//opens the mainwindow once again
+
+				//opens the main window once again
 				mainWindow.setVisible(true);
 			}
 		});
 
 	}
 
+	//gets all rows of 'currentTable'
 	public static int getRows() {
 		int rows = 0;
 		try (
@@ -295,6 +267,7 @@ public class Runner {
 
 	}
 
+	//gets all columns of 'currentTable'
 	public static int getColumns() {
 		int columns = 0;
 		try (
@@ -320,6 +293,122 @@ public class Runner {
 		}
 	}
 
+	//method returns an array of all column names based on 'currentTable'
+	public static String[] getColumnNames(int numberOfColumns) {
+		//create array to add column names to
+		//size is determined by getColumns method
+		String[] columnNames = new String[numberOfColumns];
+		try (
+				//establish connection to database
+				Connection conn = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/flight manager?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+						"root", "root");   // For MySQL only
+				//create statement object
+				Statement stmt = conn.createStatement();
+				) {
+			//create the query that will be run to database
+			String strSelect = "select * from " + currentTable;
+			//execute query
+			ResultSet rset = stmt.executeQuery(strSelect);
+			//grab the metadata from the resultSet
+			ResultSetMetaData metaData = rset.getMetaData();
+
+			//loop through the empty array of Names to add a column name based off of metaData
+			//number of loops is determined by number of columns
+			for(int i = 0; i < numberOfColumns; i++) {
+				//must use i+1 as database uses indexes starting at 1 not 0
+				columnNames[i] = metaData.getColumnName(i+1);
+			}
+			//return array of names
+			return columnNames;
+
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+			//if connection fails to establish, return null
+			//bigger problems are at hand if connection cannot be established
+			return null;
+		}
+		
+	}
+
+	//method prints out all data of a table based on the value 'currentTable'
+	public static void printData() {
+		
+		//calls the getColumns method which uses JDBC to return a tables number of columns
+		int columns = getColumns();
+
+		//calls the getRows method which uses JDBC to return a tables number of rows
+		int rows = getRows();
+
+		//hard coded array for creation of the JTable.  Eventually make into a method that pulls out the name of every column name so I can put data grabbing from database
+		//into a method of its own
+		String columnNames[] = getColumnNames(columns);
+
+		//creation of 2d array that takes in the values from the Customer Table from the database
+		Object data[][] = new String[rows][columns];
+
+		//try catch that uses the JDBC
+		try (
+				Connection conn = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/flight manager?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+						"root", "root");   // For MySQL only
+				Statement stmt = conn.createStatement();
+				) {
+			//creates a query in the database to grab all values from the Customer Table
+			String strSelect = "select * from " + currentTable;
+			ResultSet rset = stmt.executeQuery(strSelect);
+
+			//variables used to add data to the 2D array
+			int column = 0;
+			int row = 0;
+
+			//data is printed out row by row, loops through the data grabbed from database and adds it to the 2D array
+			//this loop skips to the next row each time it iterates
+			while(rset.next()) { 
+				//loop for adding the data to each column, loops 11 times for each row
+				while(column != columns) {
+					data[row][column] = rset.getString(column+1);
+					column++;
+				}
+				//resets column data for the next row, increase row with the while loop
+				column = 0;
+				row++;
+			} 
+
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+
+		//creates the JTable the data in inputed to
+		JTable dataTable = new JTable(data, columnNames);
+
+		//adds a header to remain visible wherever the user is scrolled to in the table
+		JTableHeader header = dataTable.getTableHeader();
+
+		//sets the header background to green
+		header.setBackground(Color.green);
+
+		//creates a scroll bar to be added to the table
+		JScrollPane scrollPane = new JScrollPane(dataTable);
+
+		//sets the table dimensions in the window, resizing is off so buttons aren't covered
+		dataTable.setPreferredScrollableViewportSize(new Dimension(825,520));
+		dataTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+		//adds scroll bar to table so user can see all data
+		textPanel.add(scrollPane);
+
+		//sets how many rows and columns are visible, along with coordinates of the table (top left for our case)
+		dataTable.setBounds(0,0,10,20);
+		scrollPane.setViewportView(dataTable);
+
+		//adds the table to the window
+		secondWindow.add(textPanel, BorderLayout.WEST);
+		
+		
+	}
+	
+	
 }
 
 
